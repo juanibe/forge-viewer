a3d4aa0f12ad64bb5e028d3c4018a01b2e235899
Juan Ignacio Benito <benitojuanignacio@gmail.com>
2022-10-31 17:44:09 +0000

    Testing message

--- config.js
+++ config.js
@@ -18,16 +18,22 @@
 
 // Autodesk Forge configuration
 module.exports = {
-    // Set environment variables or hard-code here
-    credentials: {
-        client_id: process.env.FORGE_CLIENT_ID,
-        client_secret: process.env.FORGE_CLIENT_SECRET,
-        callback_url: process.env.FORGE_CALLBACK_URL
-    },
-    scopes: {
-        // Required scopes for the server-side application
-        internal: ['bucket:create', 'bucket:read', 'data:read', 'data:create', 'data:write'],
-        // Required scope for the client-side viewer
-        public: ['viewables:read']
-    }
+  // Set environment variables or hard-code here
+  credentials: {
+    client_id: "ua8UGIheoZVQENGHjRqy8aFnEikGQJ3g", //process.env.FORGE_CLIENT_ID,
+    client_secret: "9ZVOQ4MaD10lCnAe", // process.env.FORGE_CLIENT_SECRET,
+    callback_url: process.env.FORGE_CALLBACK_URL,
+  },
+  scopes: {
+    // Required scopes for the server-side application
+    internal: [
+      "bucket:create",
+      "bucket:read",
+      "data:read",
+      "data:create",
+      "data:write",
+    ],
+    // Required scope for the client-side viewer
+    public: ["viewables:read"],
+  },
 };
--- server.js
+++ server.js
@@ -16,24 +16,47 @@
 // UNINTERRUPTED OR ERROR FREE.
 /////////////////////////////////////////////////////////////////////
 
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 
 const PORT = process.env.PORT || 3000;
-const config = require('./config');
-if (config.credentials.client_id == null || config.credentials.client_secret == null) {
-    console.error('Missing FORGE_CLIENT_ID or FORGE_CLIENT_SECRET env. variables.');
-    return;
+const config = require("./config");
+if (
+  config.credentials.client_id == null ||
+  config.credentials.client_secret == null
+) {
+  console.error(
+    "Missing FORGE_CLIENT_ID or FORGE_CLIENT_SECRET env. variables."
+  );
+  return;
 }
 
 let app = express();
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json({ limit: '50mb' }));
-app.use('/api/forge/oauth', require('./routes/oauth'));
-app.use('/api/forge/oss', require('./routes/oss'));
-app.use('/api/forge/modelderivative', require('./routes/modelderivative'));
+app.use(express.static(path.join(__dirname, "public")));
+
+// app.use("/", async function (req, res, next) {
+//   // for example, get a parameter:
+//   const urn = req.query.urn;
+
+//   console.log("URN", urn);
+
+//   if (true) {
+//     express.static(path.join(__dirname, "public"))(req, res, next);
+//   } else {
+//     // do something completely different
+//     res.set("Content-Type", "text/plain");
+//     res.status(401).send("expired");
+//   }
+// });
+
+app.use(express.json({ limit: "50mb" }));
+app.use("/api/forge/oauth", require("./routes/oauth"));
+app.use("/api/forge/oss", require("./routes/oss"));
+app.use("/api/forge/modelderivative", require("./routes/modelderivative"));
 app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(err.statusCode).json(err);
+  console.error(err);
+  res.status(err.statusCode).json(err);
 });
-app.listen(PORT, () => { console.log(`Server listening on port ${PORT}`); });
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
